ceb5a81668ade4b7e113cfc0820d3e0b8b254da4
Luis Zabala <zabalaluis1234@gmail.com>
2021-06-18 05:10:57 -0300

    Basic rutas

---
+++ spa/src/app.routes.ts
@@ -0,0 +1,18 @@
+import { RouterModule, Routes } from "@angular/router";
+
+//components
+import { HomeComponent } from "./app/components/home/home.component";
+import {AboutComponent} from './app/components/about/about.component';
+import {HeroesComponent} from './app/components/heroes/heroes.component';
+
+
+const APP_ROUTES : Routes = [
+
+    {path : 'home', component: HomeComponent },
+    {path : 'about', component: AboutComponent },
+    {path : 'heroes', component: HeroesComponent },
+    {path : '**', pathMatch: 'full', redirectTo: 'home'}
+]
+
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
